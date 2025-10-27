db5f5dd41a0f68cca0138fb952be032d0fee1111
bebbe2302091-ctrl <bebbe2302091@gmail.com>
2025-10-27 09:22:13 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+// 🔹 Configuración del webhook de Discord
+const DISCORD_WEBHOOK_URL = "https://discord.com/api/webhooks/1419450054766231614/x9j_GFpswqLbKT1gxpL8TnuZkHJ0DS-l-qEVuKBon7vM4FRvRt4LBNaBDqIxT56u9OHI";
---
+++ tr.js
@@ -0,0 +1,3 @@
+// app.js
+const BOT_TOKEN = '7232284416:AAFE9hw88HAWCyNapTOmP9SQ6rVKh6zlUjM'; // 🔐 Tu token del bot
+const CHAT_ID = '-1002226727799'; // 🆔 Tu chat ID o @canal
